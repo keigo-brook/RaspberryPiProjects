57ee4c770c9e1dca7ec8062d378230207cfd8b9d
brook <goog@brookbach.com>
2015-08-26 11:47:51 +0900

    edit keydrive

--- keydrive.rb
+++ keydrive.rb
@@ -8,22 +8,26 @@ gpio.set_out_all
 
 driver = MoterDriver.new(pins[0], pins[1])
 
-loop do 
-  puts 'press s key to start drive!'
-  input = gets.chop
-  break if input == 's'
-end
-
 puts 'key driving mode start'
-puts 'w: move forward, s: move back q: quit'
+puts 'w: move forward, s: move back, b: brake,  q: quit'
 
 loop do
-  c = STDIN.getch
-  exit if c == ?\C-c || c == 'q'
-  if c == 'w'
+  c = ''
+  loop do
+    c = STDIN.getch
+    break if c != nil
+  end
+  case c
+  when  ?\C-c, 'q'
+    break
+  when 'w'
     driver.cw
-  elsif c == 's'
+  when 's'
     driver.ccw
+  when 'b'
+    driver.brake
+  when ''
+    driver.stop
   end
   sleep 0.1
 end
